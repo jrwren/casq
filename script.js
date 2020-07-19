99078dc81335a1bc3595bc9a2ccb390765f8b9f2
Glitch (hello-webpage) <none>
2020-07-19 17:42:38 +0000

    🌄👲 Checkpoint ./script.js:505437/408

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 class Question {
-  constructor(q, a, b, type) {
+  constructor(q, a, b, type, point) {
     this.q = q
+    this.a = a
+    this.b = b
+    this.type = type
+    this.point = point
   }
 }
 let questions = [
-  "You get an A on a test."
+  new Question(
+    "You get an A on a test.",
+    "I am smart.",
+    "I am good in the subject that the test is in.",
+    "PVG",
+    "a"),
+  new Question(
+    "You play a game with some friends and you win.",
+    "The people that I played with did not play the game well.",
+    "I play that game well.",
+    "PSG",
+    "b"),
+  new Question()
 ]
