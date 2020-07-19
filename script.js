c76b86b2f6f2425232a5db24c73b89636fa8f526
Glitch (hello-webpage) <none>
2020-07-19 17:30:50 +0000

    👢🛳 Checkpoint ./index.html:505437/836 ./style.css:505437/40 ./script.js:505437/336

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+class Question {
+  constructor(q, a, b, type) {
+    this.q = q
+  }
+}
+let questions = [
+  "You get an A on a test."
+]
